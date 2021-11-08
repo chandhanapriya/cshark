d19c3ca7b2289ca203801e86e5552d474000eb91
chandhanapriya T <92292189+chandhanapriya@users.noreply.github.com>
2021-11-08 18:16:35 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Collections;
+
+
+namespace deserialization
+{
+         public class WeatherForecast
+        {
+            public DateTimeOffset Date { get; set; }
+            public int TemperatureCelsius { get; set; }
+            public string Summary { get; set; }
+        }
+
+        public class Program
+        {
+            public static void Main()
+            {
+                string fileName = "WeatherForecast.json";
+                string jsonString = File.ReadAllText(fileName);
+                WeatherForecast weatherForecast = JsonSerializer.Deserialize<WeatherForecast>(jsonString);
+
+                Console.WriteLine($"Date: {weatherForecast.Date}");
+                Console.WriteLine($"TemperatureCelsius: {weatherForecast.TemperatureCelsius}");
+                Console.WriteLine($"Summary: {weatherForecast.Summary}");
+            }
+        }
+    }
